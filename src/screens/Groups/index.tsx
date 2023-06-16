02057e1a429194d3dfb99d2cbd4f825f09b428c6
eduardoarad <araujocarlos893@gmail.com>
2023-06-16 11:56:32 -0300

    Config Project

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text>Groups</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+})
